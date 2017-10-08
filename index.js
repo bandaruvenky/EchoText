02882c6a0bfa2be66cdce18fd83744f581172a31
bandaruvenky <bandaruvenky9@gmail.com>
2017-10-08 10:42:19 +0530

    update index.js
    support for facebook messager

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ restService.post('/echo', function(req, res) {
     });
 });
 
+
+restService.post('/webhook', function(req, res) {
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
+});
+
+
 restService.post('/slack-test', function(req, res) {
 
     var slack_message = {
